36437e163fa716197ded7bb4f0dd277837385f39
nadirbasalamah <nadir008basalamah@gmail.com>
2022-07-07 11:28:20 +0700

    feat(config): Adding read database config from .env file. Adding read database configuration from .env file instead of hardcoded.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,72 @@
 import { PrismaService } from './../prisma/prisma.service';
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { AuthDto } from 'src/dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup() {
-    return { msg: 'I Have signed up' };
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+
+      return user;
+    } catch (error) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'credentials taken',
+          );
+        }
+      }
+
+      throw error;
+    }
   }
 
-  signin() {
-    return { msg: 'I Have signed in' };
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findFirst(
+      {
+        where: {
+          email: dto.email,
+        },
+      },
+    );
+
+    if (!user)
+      throw new ForbiddenException(
+        'credentials incorrect',
+      );
+
+    const pwMatches = await argon.verify(
+      user.hash,
+      dto.password,
+    );
+
+    if (!pwMatches)
+      throw new ForbiddenException(
+        'credentials incorrect',
+      );
+
+    delete user.hash;
+
+    return user;
   }
 }
